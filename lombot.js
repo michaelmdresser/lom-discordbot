9b02466e56d05971699cf94babe86b0374c2d2ff
Michael Dresser <michaelmdresser@gmail.com>
2016-11-23 21:08:22 -0800

    fixed dice roll and added other functions

--- lombot.js
+++ lombot.js
@@ -12,17 +12,25 @@ bot.on("message", msg => {
 
     else if (msg.content.startsWith(prefix) && msg.content.includes("d")) {
         spl = msg.content.split("d");
-        if (spl.length == 1 && msg.content[1] == "d" && Number(spl[0]) != NaN) {
-            msg.channel.sendMessage(Math.ceiling(Math.random() * Number(spl[0])));
-        } else if (spl.length == 2) {
-            var sum = 0
-            first = Number(spl[0])
-            second = Number[spl[1]]
-            if (first != NaN && second != NaN) {
-                for (i = 0; i < first; i++) {
-                    sum += Math.ceiling(Math.random() * second);
-                }
+        spl[0] = spl[0].substring(1, spl[0].length);
+        space = spl[1].search(" ");
+        if (space != -1) {
+            spl[1] = spl[1].substring(0, space);
+        }
+        console.log(spl);
+        var sum = 0;
+        var first = Number(spl[0]);
+        var second = Number(spl[1]);
+        if (spl[0] == "") {
+            first = 1;
+        }
+
+        if (first != NaN && second != NaN && Math.abs(first) < 100000) {
+            for (i = 0; i < first; i++) {
+                sum += Math.floor(Math.random() * second) + 1;
             }
+        }
+        if (sum != 0 && sum != NaN) {
             msg.channel.sendMessage(sum);
         }
     }
@@ -44,12 +52,20 @@ bot.on("message", msg => {
         msg.channel.sendMessage("Fight CU down the field,\nCU must win\nFight, fight for victory\nCU knows no defeat\nWe'll roll up a mighty score\nNever give in\nShoulder to shoulder\nWe will fight, fight\nFight, fight, fight!");
     }
 
+    if (msg.content.includes("should i do it")) {
+        msg.channel.sendMessage("do it");
+    }
+
     if (msg.content.includes("gopitt")) {
         msg.channel.sendMessage("Fuck Penn State");
     }
 
     if (msg.content.includes("fuck penn state")) {
         msg.channel.sendMessage("Fight on for dear old Pittsburgh\nAnd for the glory of the game\nShow our worthy foe that the Panther's on the go\nPitt must win today! Rah! Rah! Rah!\nCheer loyal sons of Pittsburgh\nCheer on to victory and fame\nFor the Blue and gold shall conquer as of old\nSo fight, Pitt, fight!");
+    }
+
+    if (msg.content.includes("gregory")) {
+        msg.channel.sendMessage("eyy");
     }
 
 });
