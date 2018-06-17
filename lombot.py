38f7e65144d6a27f391d4ec0973eb23f004e7246
Michael Dresser <michaelmdresser@gmail.com>
2018-06-17 02:56:52 +0000

    quality of life and storing post permalink hashes for no repeats

--- lombot.py
+++ lombot.py
@@ -8,6 +8,7 @@ import re
 
 client = discord.Client()
 sub_blacklist = []
+permalink_hashes = set()
 
 def read_blacklist():
     f = open("blacklist.txt", "r")
@@ -22,51 +23,66 @@ def add_to_blacklist(pattern):
     f.close()
 
 def subreddit_json_top(subreddit, t="day"):
-    url = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=" + t
+    url = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=&limit=20" + t
     h = {"User-Agent": "lombot v1"}
 
     response = requests.get(url, headers=h)
-    post_count = len(response.json()["data"]["children"])
+    response_children = response.json()["data"]["children"]
 
-    return response.json(), post_count
+#    unused_children = [ child for child in response_children if child["data"]["permalink"] not in permalink_hashes ]
+    unused_children = []
+    for child in response_children:
+        if "https://www.reddit.com" + child["data"]["permalink"] not in permalink_hashes:
+            unused_children.append(child)
+    post_count = len(unused_children)
+
+    return unused_children, post_count
 
 def random_from_subreddit(subreddit):
     for pattern in sub_blacklist:
         if re.search(pattern, subreddit) is not None:
             print("Subreddit {" + subreddit + "} matches blacklisted pattern {" + pattern + "}")
-            return "Blacklisted pattern"
+            return None, None, None, None
 
-    rjson, post_count = subreddit_json_top(subreddit)
+    print("trying top day")
+    unused_children, post_count = subreddit_json_top(subreddit, t="day")
 
-    if (post_count < 5):
-        rjson, post_count = subreddit_json_top(subreddit, t="week")
+    if (post_count < 1):
+        print("trying top week")
+        unused_children, post_count = subreddit_json_top(subreddit, t="week")
 
-    if (post_count < 5):
-        rjson, post_count = subreddit_json_top(subreddit, t="month")
+    if (post_count < 1):
+        print("trying top month")
+        unused_children, post_count = subreddit_json_top(subreddit, t="month")
 
-    if (post_count < 5):
-        rjson, post_count = subreddit_json_top(subreddit, t="year")
+    if (post_count < 1):
+        print("trying top year")
+        unused_children, post_count = subreddit_json_top(subreddit, t="year")
 
-    if (post_count < 5):
-        rjson, post_count = subreddit_json_top(subreddit, t="all")
+    if (post_count < 1):
+        print("trying top all")
+        unused_children, post_count = subreddit_json_top(subreddit, t="all")
 
-    post_position = random.randrange(0, post_count)
+    if (post_count < 1):
+        print("no posts on sub?")
+        return None, None, None, None
 
-    response_url = rjson["data"]["children"][post_position]["data"]["url"]
-    response_title = rjson["data"]["children"][post_position]["data"]["title"]
-    response_permalink = "https://www.reddit.com" + rjson["data"]["children"][post_position]["data"]["permalink"]
+#    post_position = random.randrange(0, post_count)
+    post = unused_children[random.randrange(0, post_count)]
 
-    return response_url, response_title, response_permalink
+    response_url = post["data"]["url"]
+    response_title = post["data"]["title"]
+    response_permalink = "https://www.reddit.com" + post["data"]["permalink"]
+    over_18 = post["data"]["over_18"]
+
+    return response_url, response_title, response_permalink, over_18
+
 
 @client.event
 async def on_message(message):
     message.content = message.content.lower()
 
     prefix = "!"
-    kys_pattern = r'k\s*y\s*s'
-    kms_pattern = r'k\s*m\s*s'
-    suicide_pattern = "suicide"
-
 
     if message.author.bot:
         return
@@ -76,22 +92,26 @@ async def on_message(message):
             if role.name == "admin":
                 add_to_blacklist(message.content[17:].strip())
                 break
-    elif re.search(kys_pattern, message.content) is not None or "kill yourself" in message.content:
-        await client.send_message(message.channel, "Please don't actually kill yourself")
-    elif re.search(kms_pattern, message.content) is not None or "kill myself" in message.content:
-        await client.send_message(message.channel, "Please don't actually kill yourself")
-    elif re.search(suicide_pattern, message.content) is not None:
-        await client.send_message(message.channel, "Please don't actually kill yourself")
     elif re.search("should .+ do it", message.content) is not None:
         await client.send_message(message.channel, "do it")
     elif re.search("should .+ play.+", message.content) is not None:
         await client.send_message(message.channel, "play it")
     elif message.content.startswith("!reddit "):
         subreddit = message.content[8:]
-        post_url, post_title, post_permalink = random_from_subreddit(subreddit)
-        embed=discord.Embed(title=post_title, url=post_permalink)
+        post_url, post_title, post_permalink, over_18 = random_from_subreddit(subreddit)
+        if (post_url is None or post_title is None or post_permalink is None):
+            await client.send_message(message.channel, "Blacklisted subreddit")
+            return
+        if over_18 and message.channel.name != "nsfw":
+            await client.send_message(message.channel, "NSFW links not allowed outside of nsfw channel")
+            return
+        embed = discord.Embed(title=post_title, url=post_permalink)
+        if "imgur.com" in post_url or "i.redd.it" in post_url or "v.redd.it" in post_url or "gfycat.com" in post_url or "giphy" in post_url:
+            print("image/gif link detected")
+            embed = embed.set_image(url=post_url)
+        print("sending permalink: %s\n\npermalink_hashes is before add: %s\n\npermalink in hashes? %s\n\n" % (post_permalink, permalink_hashes, post_permalink in permalink_hashes))
+        permalink_hashes.add(post_permalink)
         await client.send_message(message.channel, embed=embed)
-        await client.send_message(message.channel, post_url)
 
 if __name__ == "__main__":
     read_blacklist()
