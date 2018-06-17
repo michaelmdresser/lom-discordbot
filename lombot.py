245fa6c1074f578d7d4d128df9a9eed5c69bf6ba
Michael Dresser <michaelmdresser@gmail.com>
2018-06-17 05:59:36 +0000

    subreddit testing and selftexts

--- lombot.py
+++ lombot.py
@@ -28,58 +28,75 @@ def add_to_blacklist(pattern):
     logger.info("added %s to blacklist" % pattern)
 
 def subreddit_json_top(subreddit, t="day"):
-    url = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=&limit=20" + t
+    url = "https://www.reddit.com/r/" + subreddit + "/top/.json?limit=30&t=" + t
     h = {"User-Agent": "lombot v1"}
 
     response = requests.get(url, headers=h)
     response_children = response.json()["data"]["children"]
 
     unused_children = [ child for child in response_children if "https://www.reddit.com" + child["data"]["permalink"] not in permalink_hashes ]
-    # unused_children = []
-    # for child in response_children:
-    #     if "https://www.reddit.com" + child["data"]["permalink"] not in permalink_hashes:
-    #         unused_children.append(child)
     post_count = len(unused_children)
 
     return unused_children, post_count
 
+def test_subreddit(subreddit):
+    url_test = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=all&limit=1"
+    h = {"User-Agent": "lombot v1"}
+    response = requests.get(url_test, headers=h)
+    if len(response.json()["data"]["children"]) < 1:
+        return False
+    return True
+
+
 def random_from_subreddit(subreddit):
+    success = False
+    blacklisted = False
     for pattern in sub_blacklist:
         if re.search(pattern, subreddit) is not None:
             logger.info("Subreddit {" + subreddit + "} matches blacklisted pattern {" + pattern + "}")
-            return None, None, None, None
+            blacklisted = True
+
+    logger.info("testing subreddit success")
+    test_success = test_subreddit(subreddit)
+    logger.info("subreddit success = %s" % test_success)
+    if not test_success or blacklisted:
+        return None, None, None, None, None, success, blacklisted
+
 
     logger.info("trying top day")
     unused_children, post_count = subreddit_json_top(subreddit, t="day")
 
-    if (post_count < 1):
+    if (post_count < 5):
         logger.info("trying top week")
         unused_children, post_count = subreddit_json_top(subreddit, t="week")
 
-    if (post_count < 1):
+    if (post_count < 5):
         logger.info("trying top month")
         unused_children, post_count = subreddit_json_top(subreddit, t="month")
 
-    if (post_count < 1):
+    if (post_count < 5):
         logger.info("trying top year")
         unused_children, post_count = subreddit_json_top(subreddit, t="year")
 
-    if (post_count < 1):
+    if (post_count < 5):
         logger.info("trying top all")
         unused_children, post_count = subreddit_json_top(subreddit, t="all")
 
-    if (post_count < 1):
+    if (post_count < 5):
         logger.info("no posts on sub?")
-        return None, None, None, None
+        return None, None, None, None, None, success, blacklisted
+
+    success = True
 
     post = unused_children[random.randrange(0, post_count)]
 
     response_url = post["data"]["url"]
     response_title = post["data"]["title"]
     response_permalink = "https://www.reddit.com" + post["data"]["permalink"]
+    response_selftext = post["data"]["selftext"]
     over_18 = post["data"]["over_18"]
 
-    return response_url, response_title, response_permalink, over_18
+    return response_url, response_title, response_permalink, response_selftext, over_18, success, blacklisted
 
 
 @client.event
@@ -103,14 +120,16 @@ async def on_message(message):
         await client.send_message(message.channel, "play it")
     elif message.content.startswith("!reddit "):
         subreddit = message.content[8:]
-        post_url, post_title, post_permalink, over_18 = random_from_subreddit(subreddit)
-        if (post_url is None or post_title is None or post_permalink is None):
+        post_url, post_title, post_permalink, post_selftext, over_18, success, blacklisted = random_from_subreddit(subreddit)
+        if blacklisted:
             await client.send_message(message.channel, "Blacklisted subreddit")
+            return
+        if not success:
             return
         if over_18 and message.channel.name != "nsfw":
             await client.send_message(message.channel, "NSFW links not allowed outside of nsfw channel")
             return
-        embed = discord.Embed(title=post_title, url=post_permalink)
+        embed = discord.Embed(title=post_title, url=post_permalink, description=post_selftext)
         if "imgur.com" in post_url or "i.redd.it" in post_url or "v.redd.it" in post_url or "gfycat.com" in post_url or "giphy" in post_url:
             logger.info("image/gif link detected")
             embed = embed.set_image(url=post_url)
